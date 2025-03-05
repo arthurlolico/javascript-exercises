4a1dffdcd27e111ec783dd97e65d9c8054955de1
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-05 14:55:54 -0300

    03-reverseString solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverse = "";
+    let stringLength = text.length;
+    while (stringLength > 0) {
+        reverse = reverse + text[stringLength-1];
+        stringLength--;
+    }
+    return reverse;
 };
 
 // Do not edit below this line
