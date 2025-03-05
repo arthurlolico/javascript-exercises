e9b4a1752be901885dd196b1cc24e55cbc5ab408
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-05 15:28:57 -0300

    04-removeFromArray solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,element) {
+    let i = 0;
+    newArray = array;
+    for(i = 1; i < arguments.length; i++){
+        for(j = 0; j < array.length; j++){
+            if(newArray[j] === arguments[i]){
+                newArray.splice(j,1);
+                j--;
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
