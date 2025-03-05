72643e03d4d718d4053386cd9ee621118f9b3253
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-05 16:08:38 -0300

    05-sumAll solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(smallestNumber,highestNumber) {
+    let i = 0;
+    let sum = 0;
+    let aux = 0;
+    if(smallestNumber < 0 || highestNumber < 0 ||
+        !Number.isInteger(smallestNumber) || !Number.isInteger(highestNumber)
+    ) 
+        return "ERROR";
+    else {
+        if (smallestNumber > highestNumber) {
+            aux = smallestNumber;
+            smallestNumber = highestNumber;
+            highestNumber = aux;
+        }       
+        for(i = smallestNumber; i <= highestNumber ; i++){
+            sum = sum + i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
