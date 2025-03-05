be4089ee836cb90fa501a1fcfb864713cf6aa884
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-05 12:21:12 -0300

    02-repeatString solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,numberOfTimes) {
+    let combinedString = "";
+    if(numberOfTimes > -1){
+        for(i = 0; i < numberOfTimes; i++) {
+            combinedString = combinedString + string;
+        }
+    }
+    else{
+        combinedString = "ERROR";
+    }
+    return combinedString;
 };
 
 // Do not edit below this line
