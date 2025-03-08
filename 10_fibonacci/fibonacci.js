4a10be414b85861a81ddf650f00a6a943070dead
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-08 08:00:22 -0300

    10-fibonacci exercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(nthTerm) {
+    if(nthTerm < 0)
+        return "OOPS";
+    else {
+        let arrayFibonacci = [0,1];
+        for(let i = 0;i < nthTerm - 1; i++) {
+            arrayFibonacci.push(arrayFibonacci[i] + arrayFibonacci[i+1]);
+        }
 
-};
+        return arrayFibonacci[nthTerm];
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
