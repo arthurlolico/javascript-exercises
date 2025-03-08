a8a440a2582306aa67b731cce4e0cf3c17716ba3
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-07 21:24:02 -0300

    08-calculator exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x,y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const sumOfAll = arr.reduce((total,currentNumber) => {
+    return total + currentNumber;
+  },0);
+	return sumOfAll;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const multOfAll = arr.reduce((total,currentNumber) => {
+    return total * currentNumber;
+  },1);
+  return multOfAll;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(x === 0 || x === 1)
+    return 1;
+  else {
+    let fact = 1;
+    let i = 1;
+    while (i < x) {
+      fact = fact*(i + 1);
+      i++;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
