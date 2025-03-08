f02e95cbbcd86fa315dd574c05a41447dd7492bf
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-08 08:52:49 -0300

    12-findTheOldest exercise done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayPeople) {
 
+    arrayAges = arrayPeople.map((person) => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        return person.yearOfDeath - person.yearOfBirth;
+    })
+
+    let maxIndex = arrayAges.indexOf(Math.max(...arrayAges));
+    return arrayPeople[maxIndex];
 };
 
 // Do not edit below this line
