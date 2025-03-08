f1ec218bfc592cc7fd546658408cb69c9dc29cad
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-07 22:52:35 -0300

    09-palindromes exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replace(/[\s,\.!]/g, "");
+    let flag = 1;
+    let i = 0;
 
+
+    let strLength = newString.length;
+    if(strLength%2 === 0) {
+        while(i < strLength && flag === 1) {
+            if(newString[i] !== newString[strLength - 1])
+                flag = 0;
+            i++;
+            strLength--;
+        }
+    }
+    else {
+        while(i <= strLength && flag === 1) {
+            if(newString[i] !== newString[strLength - 1])
+                flag = 0;
+            i++;
+            strLength--;
+        }
+    }
+    if (flag === 1)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
