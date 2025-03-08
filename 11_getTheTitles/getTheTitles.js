49762fa40f30fb1308dafa9e7792c9a728626be2
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-08 08:25:21 -0300

    11-getTheTitles exercise done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    arrayBooks = books.reduce((array,currentItem) => {
+        array.push(currentItem.title);
+        return array;
+    },[]);
+    return arrayBooks;
 };
 
 // Do not edit below this line
